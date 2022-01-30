4d2925cfcba03676401529dd6191bc7d89f22853
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-30 23:33:31 +0530

    [Shreya] UC1_CreateLinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedList;
+Console.WriteLine("Hello, World!");
+LinkedList1 customLinkedList = new LinkedList1();
+customLinkedList.AddNode(56);
+customLinkedList.AddNode(30);
+customLinkedList.AddNode(70);
+Console.ReadLine();
